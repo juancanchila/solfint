5a9deca553013708aeb4e96812af8326d60913fe
JLopez <Juan.canchila@gmail.com>
2025-04-08 22:56:46 -0500

    pausar la valdiacion hasta cambiar el token

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,6 +1,41 @@
 // src/routes/PrivateRoute.jsx
 import React, { useEffect, useState } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { AuthService } from '../services/authService';
+import Spinner from '../shared/components/Spinner/Spinner';
+
+const PrivateRoute = () => {
+  const [isValidating, setIsValidating] = useState(true);
+  const [isAuthorized, setIsAuthorized] = useState(false);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const valid = await AuthService.validateToken();
+      if (!valid) {
+        AuthService.logout(); // 🔐 Limpia el token si no es válido
+      }
+      setIsAuthorized(valid);
+      setIsValidating(false);
+    };
+
+    checkAuth();
+  }, []);
+
+  if (isValidating) {
+    return <Spinner size={120} message="Verificando acceso..." />;
+  }
+
+  return isAuthorized ? <Outlet /> : <Navigate to="/login" replace />;
+};
+
+export default PrivateRoute;
+
+
+
+
+/*// src/routes/PrivateRoute.jsx
+import React, { useEffect, useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
 import { AuthService } from '../services/authService';
 import Spinner from '../shared/components/Spinner/Spinner';
@@ -50,3 +85,4 @@ const PrivateRoute = () => {
 };
 
 export default PrivateRoute;
+*/
