412c1955d0af435640bae52ea7edf6a16af953bc
JLopez <Juan.canchila@gmail.com>
2025-04-10 08:51:59 -0500

    Admin

--- src/components/RoleRoute.jsx
+++ src/components/RoleRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 const RoleRoute = () => {
   const role = localStorage.getItem('role');
 console.log(role);
-  const isAdmin = role === 'admin' || role === 'Super Admin';
+  const isAdmin = role === 'Admin' || role === 'Super Admin';
 
   return isAdmin ? <Outlet /> : <Navigate to="/home" replace />;
 };
